eceb4fc676d528c0190e5f5ee84af8584c018ee0
Dustin LeBlanc <dustin.m.leblanc@gmail.com>
2017-12-01 13:54:47 -0500

    Add in all the things

---
+++ web/modules/modules/custom/big_red_bear/tests/src/Unit/LoadTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\big_red_bear\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Simple test to ensure that main page loads with module enabled.
+ *
+ * @group big_red_bear
+ */
+class LoadTest extends UnitTestCase {
+
+  /**
+   *
+   */
+  public function testTrueIsTrue() {
+    $this->assertTrue(TRUE);
+  }
+
+}
